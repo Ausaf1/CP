cf1233bf86f109fdf8c2426a3eeecc17983270c9
Ausaf1 <76591155+Ausaf1@users.noreply.github.com>
2021-10-31 19:06:21 +0530

    Time: 56 ms (63.98%), Space: 26 MB (94.22%) - LeetHub

---
+++ capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int check(vector<int>& weights, int ship_capacity){
+        int number_of_ships_needed =1;
+        int currently_filled = 0;
+        for(int i = 0;i<weights.size();i++){
+            if(ship_capacity-currently_filled>=weights[i]){
+                currently_filled += weights[i];
+            }
+            else{
+                number_of_ships_needed++;
+                currently_filled = weights[i];
+            }
+        }
+        return number_of_ships_needed;
+    }
+    int shipWithinDays(vector<int>& weights, int days) {
+        int lower_limit = *max_element(weights.begin(),weights.end());
+        int upper_limit = accumulate(weights.begin(),weights.end(),0);
+        while(lower_limit<upper_limit){
+           int mid = (lower_limit+upper_limit)/2;
+            if(check(weights,mid)>days){
+                lower_limit = mid+1;
+            }
+            else{
+                upper_limit = mid;
+            }
+        }
+        return lower_limit;
+    }
+};
