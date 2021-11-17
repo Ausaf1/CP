df564277349cd737601af9daf5504ecc375835a0
Ausaf1 <76591155+Ausaf1@users.noreply.github.com>
2021-11-17 19:57:20 +0530

    Time: 4 ms (87.44%), Space: 9.6 MB (43.54%) - LeetHub

--- two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -1,16 +1,41 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
+        // int l = 0,r = numbers.size()-1;
+        // while(l<r){
+        //     if(numbers[l]+numbers[r] == target){
+        //         return {l+1,r+1};
+        //     }
+        //     else if(numbers[l]+numbers[r] > target){
+        //         r--;
+        //     }
+        //     else{
+        //         l++;
+        //     }
+        // }
+        // return {};
         int l = 0,r = numbers.size()-1;
+        sort(numbers.begin(),numbers.end());
         while(l<r){
-            if(numbers[l]+numbers[r] == target){
-                return {l+1,r+1};
-            }
-            else if(numbers[l]+numbers[r] > target){
-                r--;
-            }
-            else{
-                l++;
+          int mid = (l+r)/ 2;
+            if (numbers[l] + numbers[r] == target) {
+                return {l + 1, r + 1};
+            } else if (numbers[l] + numbers[r] < target) {
+                if (numbers[mid] + numbers[r] == target) {
+                    return {mid + 1, r + 1};
+                } else if (numbers[mid] + numbers[r] < target) {
+                    l = mid + 1;
+                } else {
+                    l++;
+                }
+            } else {    
+                if (numbers[l] + numbers[mid] == target) {
+                    return {l + 1, mid + 1};
+                } else if (numbers[l] + numbers[mid] > target) {
+                    r = mid - 1;
+                } else {
+                    r--;
+                }
             }
         }
         return {};
