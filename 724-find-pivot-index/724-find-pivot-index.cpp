040bc293815b9435c65f794521b4c5b6907ea761
Ausaf1 <76591155+Ausaf1@users.noreply.github.com>
2022-01-07 23:34:35 +0530

    Time: 42 ms (15.78%), Space: 31.1 MB (64.39%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.cpp
+++ 724-find-pivot-index/724-find-pivot-index.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
      int l = 0,r = 0;
+        if(nums.size()==1)
+            return 1;
         for(int i = 0;i < nums.size();i++){
             r+=nums[i];
         }
