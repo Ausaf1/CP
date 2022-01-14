032f2a0612eefd65b655f6d5ab2dc040d068f4d7
Ausaf1 <76591155+Ausaf1@users.noreply.github.com>
2022-01-15 00:07:21 +0530

    Time: 514 ms (5.10%), Space: 94.3 MB (5.00%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,15 +1,24 @@
 class Solution {
 public:
+    int helper(int amount,int n,vector<int>& coins,vector<vector<int>>& dp){
+        if(n<=0) return INT_MAX-1;
+        if(amount == 0)
+            return 0;
+        if(dp[amount][n]!=-1)
+            return dp[amount][n];
+        if(coins[n-1]<=amount)
+            return dp[amount][n] = min(1+helper(amount-coins[n-1],n,coins,dp),helper(amount,n-1,coins,dp));
+        else
+            return dp[amount][n] = helper(amount,n-1,coins,dp);
+    }
+    
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<int> dp(amount+1,amount+1);
-        dp[0] = 0;
-        for(int i = 1; i < dp.size();i++){
-            for(int j = 0;j < n;j++){
-                if(coins[j] <= i)
-                    dp[i] = min(dp[i], dp[i-coins[j]]+1);
-            }
-        }
-        return dp[amount] > amount ? -1 : dp[amount];
+        vector<vector<int>> dp(amount+1,vector<int>(n+1,-1));
+        int minimum = helper(amount,n,coins,dp);
+        if(minimum == INT_MAX-1)
+            return -1;
+        else
+            return minimum;
     }
 };
