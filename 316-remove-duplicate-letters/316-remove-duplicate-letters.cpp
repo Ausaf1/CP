24eee8a0f1dcb526be5ebc320c6f625fc40b0157
Ausaf1 <76591155+Ausaf1@users.noreply.github.com>
2022-03-18 17:36:05 +0530

    Time: 10 ms (13.47%), Space: 6.6 MB (81.09%) - LeetHub

---
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        vector<int> count( 26,0),visited(26,0);
+        string ans =  "";
+        int n = s.length();
+        for(int i = 0;i < n;i++){
+            count[s[i] - 'a']++;
+        }
+        for(int i = 0;i < n;i++){
+            count[s[i] - 'a']--;
+            if(!visited[s[i] - 'a']){
+                while(ans.size() > 0 && ans.back() > s[i] && count[ans.back() - 'a'] > 0){
+                    visited[ans.back() - 'a'] = 0;
+                    ans.pop_back();
+                }
+                ans += s[i];
+                visited[s[i] - 'a'] = 1;
+            }
+        }
+        return ans;
+    }
+};
