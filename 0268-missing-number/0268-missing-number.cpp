133f6bceb6019185b52214a7231a0d1e966a982d
Ausaf1 <76591155+Ausaf1@users.noreply.github.com>
2023-06-05 15:20:47 +0530

    Time: 17 ms (84.05%), Space: 18 MB (52.90%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int n = nums.size(),sum = 0;
-        for(int i = 0;i < n;i++) sum += nums[i];
-        return n*(n + 1)/2 - sum;
+        int n = nums.size(),xor1 = 0;
+        for(int i = 0;i < n;i++) xor1 ^= nums[i],xor1 ^= (i + 1);
+        return xor1;
     }
 };
