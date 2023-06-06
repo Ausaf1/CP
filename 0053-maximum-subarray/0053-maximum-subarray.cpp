30c1e559edf88ec62bc1998f28a1b3467339acd3
Ausaf1 <76591155+Ausaf1@users.noreply.github.com>
2023-06-07 01:20:20 +0530

    Time: 137 ms (13.57%), Space: 67.8 MB (45.88%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxSum = INT_MIN,sum = 0;
+        for(auto it : nums){
+            sum += it;
+            maxSum = max(maxSum,sum);
+            if(sum < 0) sum = 0;
+        }
+        return maxSum;
+    }
+};
