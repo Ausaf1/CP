9d2394354a57ef87980f1d8c78b438be797683f6
Ausaf1 <76591155+Ausaf1@users.noreply.github.com>
2023-07-09 20:32:39 +0530

    Time: 21 ms (75.61%), Space: 6.8 MB (87.53%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -0,0 +1,25 @@
+class Solution {
+private:
+    void helper(int left,int right,string &s,int &mxLen){
+        while(left >= 0 && right < s.size() && s[left] == s[right]){
+            if(right - left + 1 > mxLen){
+                mxLen = right - left + 1;
+                l = left;
+                r = right;
+            } 
+            left--;
+            right++;
+        }
+    }
+public:
+    int l = 0,r = 0; 
+    string longestPalindrome(string s) {
+        int n = s.length();
+        if(n == 1) return s.substr(0,1);
+        if(n == 2 && s[0] == s[1]) return s;
+        int mxLen = 1;
+        for(int i = 1;i < n-1;i++) helper(i-1,i+1,s,mxLen);
+        for(int i = 0;i < n-1;i++) helper(i,i+1,s,mxLen);
+        return s.substr(l,r-l+1);
+    }
+};
