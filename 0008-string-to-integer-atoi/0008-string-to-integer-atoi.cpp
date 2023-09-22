79bc776850665c351518d035de979b107a838e78
Ausaf1 <76591155+Ausaf1@users.noreply.github.com>
2023-09-22 14:55:09 +0530

    Time: 0 ms (100%), Space: 6.5 MB (30.79%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        int len = s.length();
-        double num = 0;
-        int i = 0;
-        while(s[i] == ' '){
-            i++;
+        int sign = 1,base = 0,i = 0;
+        while(s[i] == ' ') i++;
+        if(s[i] == '+' || s[i] == '-'){
+            sign = 1 - 2*(s[i++] == '-');
         }
-        bool positive = s[i] == '+';
-        bool negative = s[i] == '-';
-        positive == true ? i++ : i;
-        negative == true ? i++ : i;
-        while(i < len && s[i] >= '0' && s[i] <= '9'){
-            num = num*10 + (s[i] - '0');
-            i++;
+        while(i < s.length() && s[i] >= '0' && s[i] <= '9'){
+            if(base > INT_MAX/10 || (base == INT_MAX/10 && s[i] - '0' > 7)){
+                if(sign == 1) return INT_MAX;
+                else return INT_MIN;
+            }
+            base = base*10 + (s[i++] - '0');
         }
-        num = negative ? -num : num;
-        num = (num > INT_MAX) ? INT_MAX : num;
-        num = (num < INT_MIN) ? INT_MIN : num;
-        return (int)num;
+        return (int)base*sign;
     }
 };
