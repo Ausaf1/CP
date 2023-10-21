2a74857eb642362e1c9847b52fea3adb82dd86b6
Ausaf1 <76591155+Ausaf1@users.noreply.github.com>
2023-10-21 12:36:18 +0530

    Time: 189 ms (94.29%), Space: 117.3 MB (89.8%) - LeetHub

---
+++ 1286-constrained-subsequence-sum/1286-constrained-subsequence-sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int constrainedSubsetSum(vector<int>& nums, int k) {
+        int n = nums.size();
+        deque<int> maxSumQueue;
+        int maxSum = nums[0];
+        for(int i = 0;i < nums.size();i++){
+            nums[i] += !maxSumQueue.empty() ? maxSumQueue.front() : 0;
+            maxSum = max(maxSum,nums[i]);
+            while(!maxSumQueue.empty() && nums[i] > maxSumQueue.back()) maxSumQueue.pop_back();
+        if(nums[i] > 0) maxSumQueue.push_back(nums[i]);
+        if(i >= k && !maxSumQueue.empty() && maxSumQueue.front() == nums[i-k]) maxSumQueue.pop_front();
+        }
+        return maxSum;
+    }
+};
